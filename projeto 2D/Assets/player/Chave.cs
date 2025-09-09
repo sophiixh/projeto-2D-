3ceb4d9fe519c13dc8d704d5b647692a0112cb37
sophiixh <sophia.caroline@escolar.ifrn.edu.br>
2025-09-09 10:17:19 -0300

    aula 01

---
+++ projeto 2D/Assets/player/Chave.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Chave : MonoBehaviour
+{
+    public GameObject textoDeVitoria;
+
+    private void OnCollisionEnter2D(Collision2D other) 
+    {
+        if (other.gameObject.CompareTag("Player")) 
+
+      {
+         textoDeVitoria.SetActive(true);
+         gameObject.SetActive(false); 
+      }
+    
+    }
+}
